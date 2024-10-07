4fd3a1bb74f12a01a4ddeb9a608945b90855308c
AizenSouskeHado901 <aizenvolkov2@gmail.com>
2024-10-07 18:55:46 +0400

    Update test.py

--- HW1/test.py
+++ HW1/test.py
@@ -1,15 +1,17 @@
 import unittest
 from unittest.mock import patch, MagicMock
-import os
 from main import Emulator
 
 class TestEmulatorCommands(unittest.TestCase):
-
     def setUp(self):
-        # Этот метод запускается перед каждым тестом
         self.emulator = Emulator("test.tar", "test.log", "init.txt")
         
-    # Тесты команды ls
+    def test_wc_no_parameters(self):
+        with patch.object(self.emulator, 'execute_command', return_value='5 10 50') as mock_command:
+            result = self.emulator.execute_command('wc file.txt')
+            self.assertEqual(result, '5 10 50')
+            mock_command.assert_called_with('wc file.txt')
+
     def test_ls_empty_directory(self):
         with patch.object(self.emulator, 'execute_command', return_value=[]) as mock_command:
             files = self.emulator.execute_command('ls')
@@ -28,7 +30,6 @@ class TestEmulatorCommands(unittest.TestCase):
             self.assertEqual(files, ['file1.txt', 'file2.txt', 'file3.txt'])
             mock_command.assert_called_with('ls')
 
-    # Тесты команды cd
     def test_cd_to_valid_directory(self):
         with patch.object(self.emulator, 'execute_command') as mock_command:
             self.emulator.execute_command('cd /valid_directory')
@@ -45,45 +46,23 @@ class TestEmulatorCommands(unittest.TestCase):
             self.emulator.execute_command('cd ..')
             mock_command.assert_called_with('cd ..')
 
-    # Тесты команды ws (WebSocket или другое поведение)
-    # Зависит от предполагаемого поведения команды ws
-    def test_ws_connect(self):
-        with patch.object(self.emulator, 'execute_command', return_value="Connected") as mock_command:
-            response = self.emulator.execute_command('ws connect')
-            self.assertEqual(response, "Connected")
-            mock_command.assert_called_with('ws connect')
-
-    def test_ws_send_message(self):
-        with patch.object(self.emulator, 'execute_command', return_value="Message sent") as mock_command:
-            response = self.emulator.execute_command('ws send Hello')
-            self.assertEqual(response, "Message sent")
-            mock_command.assert_called_with('ws send Hello')
-
-    def test_ws_disconnect(self):
-        with patch.object(self.emulator, 'execute_command', return_value="Disconnected") as mock_command:
-            response = self.emulator.execute_command('ws disconnect')
-            self.assertEqual(response, "Disconnected")
-            mock_command.assert_called_with('ws disconnect')
-
-    # Тесты команды head
-    def test_head_single_line(self):
-        with patch.object(self.emulator, 'execute_command', return_value=['This is a line']) as mock_command:
-            lines = self.emulator.execute_command('head -n 1 file.txt')
-            self.assertEqual(lines, ['This is a line'])
-
-            mock_command.assert_called_with('head -n 1 file.txt')
-
-    def test_head_multiple_lines(self):
-        with patch.object(self.emulator, 'execute_command', return_value=['Line 1', 'Line 2', 'Line 3']) as mock_command:
-            lines = self.emulator.execute_command('head -n 3 file.txt')
-            self.assertEqual(lines, ['Line 1', 'Line 2', 'Line 3'])
-            mock_command.assert_called_with('head -n 3 file.txt')
-
-    def test_head_no_file(self):
-        with patch.object(self.emulator, 'execute_command', side_effect=FileNotFoundError("No such file")) as mock_command:
-            with self.assertRaises(FileNotFoundError):
-                self.emulator.execute_command('head file.txt')
-                mock_command.assert_called_with('head file.txt')
+    def test_head_default_lines(self):
+        mock_output = (
+            "Line 1\n"
+            "Line 2\n"
+            "Line 3\n"
+            "Line 4\n"
+            "Line 5\n"
+            "Line 6\n"
+            "Line 7\n"
+            "Line 8\n"
+            "Line 9\n"
+            "Line 10"
+        )
+        with patch.object(self.emulator, 'execute_command', return_value=mock_output) as mock_command:
+            result = self.emulator.execute_command('head file.txt')
+            self.assertEqual(result, mock_output)
+            mock_command.assert_called_with('head file.txt')
 
 if __name__ == '__main__':
     unittest.main()
